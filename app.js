4bed569aeb7b3e0ad96ddb23c0afdcf969442211
김재승 <jaes@gmail.com>
2025-02-20 10:40:35 +0900

    요총 err예외처리

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ import http from "http";
 
 const server = http.createServer(function (request, response) {
     if (request.method === "GET") {
-        if (request.url === "/") {
+        if (request.url === "/1") {
             const mainPage = fs.readFileSync("index.html");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end(mainPage);
+        } else {
+            // 서버 요청 예외처리
+            const mainPage = fs.readFileSync("err.html");
             response.writeHead(200, { "content-type": "text/html" });
             response.end(mainPage);
         }
     }
     if (request.method === "POST") {
+        if (request.url === "/1") {
+            const mainPage = fs.readFileSync("index.html");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end(mainPage);
+        } else {
+            // 서버 요청 예외처리
+            const mainPage = fs.readFileSync("err.html");
+            response.writeHead(200, { "content-type": "text/html" });
+            response.end(mainPage);
+        }
     }
 });
 
