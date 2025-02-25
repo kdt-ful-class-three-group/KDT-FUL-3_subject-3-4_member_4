05073d4ab25911f863b0d1d0892a9c9e0ff496c0
김재승 <jaes@gmail.com>
2025-02-25 16:13:02 +0900

    프로젝트 완성

--- components/readFile.js
+++ components/readFile.js
@@ -5,11 +5,10 @@ function readFile() {
     if (dataJsonFile !== "") {
         const dataObj = JSON.parse(dataJsonFile);
         dataObj.forEach((element) => {
-            // console.log(element);
             arrJson.push(element);
         });
     }
     return arrJson;
 }
-
+//* 서버 껐다 켰을 때 json데이터가 비어있으면 패스 / 파일 저장 해서 서버 껐다 켰을 때 데이터 유지 시키는 로직
 export { readFile };
