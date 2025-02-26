7390fb9b578abb282550036a7675292e0b3332a0
김재승 <jaes@gmail.com>
2025-02-26 15:14:58 +0900

    컴포넌트 나누는중

---
+++ components/js/updateData.js
@@ -0,0 +1,37 @@
+function updateData(data, index) {
+    console.log(index);
+    const id = document.getElementById("dataId").innerText;
+    const inputName = document.getElementById("inputName").value;
+    const timestamp = document.getElementById("dataTime").innerText;
+
+    const updatedInfo = {
+        id: id,
+        inputName: inputName,
+        timestamp: timestamp,
+    };
+    console.log(updatedInfo); // 수정해서 jsons에 넣을 객체
+
+    // fetch(`http://localhost:8000/data.json`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    location.href = `/`;
+    const index = data.findIndex((item) => item.id == id);
+    if (index !== -1) {
+        data[index] = updatedInfo; // 해당 index에 바꿔치기
+        fetch(`http://localhost:8000/update`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data, null, 2),
+        }).catch((error) => {
+            console.log("에러" + error);
+        });
+    }
+    // })
+    // .catch((error) => {
+    //     console.log("에러" + error);
+    // });
+}
+
+export { updateData };
